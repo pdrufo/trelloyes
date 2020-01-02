37e0b78c0a02a16147eb443c6ca6cde8c1f59371
Peter Rufo <peter@peterrufo.com>
2020-01-02 11:41:01 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import store from './store';
+
+
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
